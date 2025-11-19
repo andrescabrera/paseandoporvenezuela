9facce11e3558757ff97080501d7468f1dc98a16
Andrés Cabrera <andrescabrera@users.noreply.github.com>
2025-11-18 20:39:51 -0400

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Hero from './components/Hero';
-import Hotels from './components/Hotels';
-import Gallery from './components/Gallery';
-import About from './components/About';
-import Footer from './components/Footer';
 import HotelDetail from './pages/HotelDetail';
 import Landing from './pages/Landing';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/paseandoporvenezuela">
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/hoteles/:slug" element={<HotelDetail />} />
